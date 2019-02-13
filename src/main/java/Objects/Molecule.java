808b90ca25add1d67872b9bf723c489577ac1fe1
Shazuli <sigge-e@hotmail.com>
2019-02-13 14:21:06 +0100

    Improved drawing of Molecule object.

--- src/main/java/Objects/Molecule.java
+++ src/main/java/Objects/Molecule.java
@@ -3,8 +3,11 @@ package main.java.Objects;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
+import java.awt.*;
+
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,23 +39,32 @@ public class Molecule {
     }
     public Canvas draw(int xOffset, int yOffset) {
         Canvas canvas = new Canvas(400,70);
+        canvas.setTranslateX(xOffset);
+        canvas.setTranslateY(yOffset);
+        int sizeBig = 40;
+        int sizeSmall = 19;
         GraphicsContext gc = canvas.getGraphicsContext2D();
         //gc.setFill(Color.GREEN);
+        AffineTransform affinetransform = new AffineTransform();
+        FontRenderContext frc = new FontRenderContext(affinetransform,true,true);
+        Font fontBig = new Font(gc.getFont().getName(),Font.PLAIN,sizeBig);
+        Font fontSmall = new Font(gc.getFont().getName(),Font.PLAIN,sizeSmall);
         double offset = 0;
         for (int i=0;i<this.elements.size();i++) {
             Element e = this.elements.get(i);
-            double x = i*30+offset;
-            double y = canvas.getHeight()*0.5-4;
-            gc.setFont(new Font(40));
-            gc.fillText(e.getName(),x,y);
+            double widthBig = fontBig.getStringBounds(e.getName(),frc).getWidth();
+            gc.setFont(new javafx.scene.text.Font(sizeBig));
+            gc.fillText(e.getName(),offset,canvas.getHeight()*0.5-4);
+            offset += widthBig+2;
+
             if (e.getQuantity() > 1) {
-                gc.setFont(new Font(17));
-                gc.fillText(e.getQuantity() + "", x + 27, y + 2);
-                offset += 8*String.valueOf(e.getQuantity()).length()+2;
+                gc.setFont(new javafx.scene.text.Font(sizeSmall));
+                double widthSmall = fontSmall.getStringBounds(String.valueOf(e.getQuantity()),frc).getWidth();
+                gc.fillText(String.valueOf(e.getQuantity()),offset-2,canvas.getHeight()*0.5+2);
+                offset += widthSmall;
             }
-            //newX = (x+offset)*8;
         }
-        //canvas.setWidth(newX);
+        canvas.setWidth(offset);
 
         return canvas;
     }
